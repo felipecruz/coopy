2f412d6d98e6b03c1e3997d6acb0d15cace12e28
Felipe cruz <felipecruz@loogica.net>
2012-08-28 23:34:22 -0300

    remove trailing spaces

--- coopy/utils.py
+++ coopy/utils.py
@@ -4,11 +4,11 @@ def method_or_none(instance, name):
                                     not callable(method) :
         return None
     return method
-       
+
 def action_check(obj):
-    return (hasattr(obj, '__readonly'), 
+    return (hasattr(obj, '__readonly'),
             hasattr(obj, '__unlocked'),
-            hasattr(obj, '__abort_exception')) 
+            hasattr(obj, '__abort_exception'))
 
 def inject(obj, name, dependency):
     obj.__dict__[name] = dependency
