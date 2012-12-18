74650514cacc162c21e517fcbafd997a1e572db8
Felipe cruz <felipecruz@loogica.net>
2012-12-18 16:52:19 -0200

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import find_packages
 from distutils.core import setup
 
 setup(name='coopy',
-      version='0.3',
-      description='coopy - plain objects prevalence tool',
+      version='0.3.1',
+      description='coopy - plain objects persistence/prevalence tool',
       author='Felipe Cruz',
       author_email='felipecruz@loogica.net',
       url='http://coopy.readthedocs.org',
