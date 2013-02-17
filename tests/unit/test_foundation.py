edd1e0fab053598af540346095b83d829fcee8af
Felipe cruz <felipecruz@loogica.net>
2013-02-16 23:57:01 -0200

    Update Clocks Tests

--- tests/unit/test_foundation.py
+++ tests/unit/test_foundation.py
@@ -25,7 +25,7 @@ class TestAction(unittest.TestCase):
         inject(dummy, '_clock', RecordClock())
 
         action = Action('caller_id',
-                        'business_method_noargs', 
+                        'business_method_noargs',
                         datetime.now(),
                         (),
                         {})
@@ -35,7 +35,7 @@ class TestAction(unittest.TestCase):
         self.assertEquals(1, dummy.exec_count)
 
         action = Action('caller_id',
-                        'business_method_args', 
+                        'business_method_args',
                         datetime.now(),
                         ([1]),
                         {})
@@ -45,7 +45,7 @@ class TestAction(unittest.TestCase):
         self.assertEquals(3, dummy.exec_count)
 
         action = Action('caller_id',
-                        'business_method_kwargs', 
+                        'business_method_kwargs',
                         datetime.now(),
                         (),
                         {'keyword_arg' : 'test'})
@@ -59,28 +59,34 @@ class TestRecordClock(unittest.TestCase):
         from coopy.foundation import RecordClock
 
         clock = RecordClock()
-        self.assertTrue(len(clock.timestamps) == 0)
+        self.assertTrue(len(clock.results) == 0)
 
         dt1 = clock.now()
-        self.assertEquals(dt1, clock.timestamps[0])
+        self.assertEquals(dt1, clock.results[0])
 
         dt2 = clock.now()
-        self.assertEquals(dt2, clock.timestamps[1])
+        self.assertEquals(dt2, clock.results[1])
+
+        dt = clock.today()
+        self.assertEquals(dt, clock.results[2])
+
+        utcnow = clock.utcnow()
+        self.assertEquals(utcnow, clock.results[3])
 
 class TestRestoreClock(unittest.TestCase):
     def test_restore_clock(self):
         from coopy.foundation import RestoreClock
-        from datetime import datetime
+        from datetime import datetime, date
 
         dt1 = datetime.now()
-        dt2 = datetime.now()
-        dt3 = datetime.now()
-        
+        dt2 = date.today()
+        dt3 = datetime.utcnow()
+
         clock = RestoreClock([dt1, dt2, dt3])
 
         self.assertEquals(dt1, clock.now())
-        self.assertEquals(dt2, clock.now())
-        self.assertEquals(dt3, clock.now())
+        self.assertEquals(dt2, clock.today())
+        self.assertEquals(dt3, clock.utcnow())
 
 if __name__ == "__main__":
     unittest.main()
